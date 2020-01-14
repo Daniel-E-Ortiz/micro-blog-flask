39ad841faa38d5375c7f4150eef6c261ff6b4615
Daniel Ortiz <danielson.ortiz@outlook.com>
2020-01-14 12:08:21 -0600

    Added Microblog.py and implemented HelloWorld!

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 
-app = Flask(__name__)       ''' creates an app obj as an instance
-                                of class Flask imported from flask package.'''
+''' 
+    This creates an app obj as an instance
+    of class Flask imported from flask package.
+'''
+app = Flask(__name__)       
 
-from app import routes      ''' imported at the bottom as a workaround to circular imports,
-                                a common problem with Flask apps.'''
+''' 
+    routes is imported at the bottom as a workaround to circular imports,
+    a common problem with Flask apps.
+'''
+from app import routes      
--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,13 @@
 '''
 from app import app
 
+'''
+    @app.route() is a decorator that creates an association between the URL
+    given as an argument and the function. 
+    When the web browser requests either of these URL's then Flask is going
+    to invoke this function and pass the return value of it back to the browser.
+'''
 @app.route('/')
-@app.route('/index')
+@app.route('/index') 
 def index():
     return "Hello, World!"
